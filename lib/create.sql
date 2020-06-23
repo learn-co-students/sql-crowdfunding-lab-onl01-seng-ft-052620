6b8f8fd2d220b389ad131d3f970d835fd6280c06
Luca <lucakanyo@protonmail.com>
2020-06-23 13:22:37 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,27 @@
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY, 
+title TEXT,
+category TEXT,
+funding_goal TEXT,
+start_date NUMERIC,
+end_date NUMERIC
+);
+
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+
+CREATE TABLE pledges(
+    id INTEGER PRIMARY KEY, 
+    amount INTEGER, 
+    user_id INTEGER, 
+    project_id INTEGER
+);
+
+
+
+
+
